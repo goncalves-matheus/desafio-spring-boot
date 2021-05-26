d136fcc680d2e5bea55b5324de7a2088ab4a4642
= <=>
2021-05-26 10:43:54 -0300

    Adição da ApiInfo no swagger

--- rabbitMQ/catalogo-de-produtos/src/main/java/br/com/compasso/catalogodeprodutos/config/SwaggerConfig.java
+++ rabbitMQ/catalogo-de-produtos/src/main/java/br/com/compasso/catalogodeprodutos/config/SwaggerConfig.java
@@ -1,10 +1,15 @@
 package br.com.compasso.catalogodeprodutos.config;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,7 +25,24 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("br.com.compasso.catalogodeprodutos"))
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
-                //.ApiInfo(metaInfo());
+                .build()
+                .apiInfo(metaInfo());
+                
     }
+
+    private ApiInfo metaInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Catálogo de Produto",
+                "API REST Java Spring Boot",
+                "1.0",
+                null, 
+                new Contact("Matheus Gonçalves", 
+                            "https://github.com/Matheus729/desafio-spring-boot",
+                            "matheus.goncalves@compasso.com.br"),
+                null,
+                null, new ArrayList<VendorExtension>()
+        );
+        return apiInfo;
+    }
+    
 }
